e4541139f5cda78f769335d6f4c863f249dd444b
AkimGomes <ak-gomes@hotmail.com>
2023-05-29 15:12:51 -0300

    feat(orcamento, cliente/ models) - Criando as tabelas de cliente e orcamento

--- cliente/models.py
+++ cliente/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=14)  # Considerando o formato XXX.XXX.XXX-XX
+    telefone_1 = models.CharField(max_length=15)  # Considerando o formato DDD + número (exemplo: 11 99999-9999)
+    telefone_2 = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField()
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+    ativo_inativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
--- orcamento/models.py
+++ orcamento/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from cliente.models import Cliente
 
-# Create your models here.
+
+class Orcamento(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField()
+    cliente_orcamento = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    observacao = models.TextField(blank=True)
+    data_orcamento = models.DateTimeField()
+    ativo_inativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome

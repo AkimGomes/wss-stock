4bbbb841c542a702b43027768cb44482880802cc
Akim Gomes <ak-gomes@hotmail.com>
2024-08-26 15:00:21 -0300

    refact(settings) - alterando DEFAULT_AUTO_FIELD

--- setup/settings.py
+++ setup/settings.py
@@ -136,7 +136,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),

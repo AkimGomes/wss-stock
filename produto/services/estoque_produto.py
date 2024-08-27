b072a56ccdc6ad29b49fee963aff12d902b0e0ea
Akim Gomes <ak-gomes@hotmail.com>
2024-08-27 14:51:13 -0300

    refact(produto/services) - alterando nome de variavel errada

--- produto/services/estoque_produto.py
+++ produto/services/estoque_produto.py
@@ -5,7 +5,7 @@ class EstoqueProdutoService:
     estoque_de_produto_repositorio_escrita: RepoEstoqueProdutoEscrita
 
     def __init__(self):
-        self.produto_repositorio = RepoEstoqueProdutoEscrita()
+        self.estoque_de_produto_repositorio_escrita = RepoEstoqueProdutoEscrita()
 
     def atualizar_estoque_de_produto_se_necessario(self, instance, quantidade: int):
         if quantidade is not None and quantidade != instance.estoque_produto.quantidade:

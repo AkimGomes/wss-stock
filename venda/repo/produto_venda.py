63fe5b2dcb22d86836802b583b4fe026ba2c268a
Akim Gomes <ak-gomes@hotmail.com>
2024-09-02 17:59:14 -0300

    chore(tests/venda) - retirando código morto

--- venda/repo/produto_venda.py
+++ venda/repo/produto_venda.py
@@ -2,14 +2,6 @@ from produto.models import Produto
 from venda.models import ProdutoVenda
 
 
-class RepoProdutoVendaLeitura:
-
-    @staticmethod
-    def consultar_venda_pela_observacao(observacao: str) -> QuerySet[ProdutoVenda]:
-        venda = Venda.objects.filter(observacao__icontains=observacao)
-        return venda
-
-
 class RepoProdutoVendaEscrita:
 
     @staticmethod
